8eb6986277dd7c717494b5e302f66a991b29e994
Mark Du <dulongjiebackup@gmail.com>
2023-09-11 14:28:18 -0400

    fix type mismatch size with size[]

--- actions/get-colors.tsx
+++ actions/get-colors.tsx
@@ -2,7 +2,7 @@ import { Color } from "@/type";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/colors`
 
-const getColors =async ():Promise<Color> => {
+const getColors =async ():Promise<Color[]> => {
     const result = await fetch(URL)
     return result.json()
 }
--- actions/get-sizes.tsx
+++ actions/get-sizes.tsx
@@ -2,7 +2,7 @@ import { Size } from "@/type";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/sizes`
 
-const getSizes =async ():Promise<Size> => {
+const getSizes =async ():Promise<Size[]> => {
     const result = await fetch(URL)
     return result.json()
 }
