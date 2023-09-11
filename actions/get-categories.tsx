cce3739ed33291f4a3888dc1e880508b68d669ec
Mark Du <dulongjiebackup@gmail.com>
2023-09-11 14:40:39 -0400

    recorrect category[], was in wrong place

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -2,7 +2,7 @@ import { Category } from "@/type";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`
 
-const getCategories =async ():Promise<Category> => {
+const getCategories =async ():Promise<Category[]> => {
     const result = await fetch(URL)
     return result.json()
 }
--- actions/get-category.tsx
+++ actions/get-category.tsx
@@ -2,7 +2,7 @@ import { Category } from "@/type";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`
 
-const getCategory =async (id: string):Promise<Category[]> => {
+const getCategory =async (id: string):Promise<Category> => {
     const result = await fetch(`${URL}/${id}`)
     return result.json()
 }
